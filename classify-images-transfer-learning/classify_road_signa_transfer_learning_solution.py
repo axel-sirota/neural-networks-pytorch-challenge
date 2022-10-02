8d6ab27e6bafb2b46e1ce52bf4b20e52d07a959d
Axel Sirota <axel@enginzyme.com>
2022-10-02 18:34:10 -0300

    gt solution

--- classify-images-transfer-learning/classify_road_signa_transfer_learning_solution.py
+++ classify-images-transfer-learning/classify_road_signa_transfer_learning_solution.py
@@ -3,12 +3,8 @@ import os
 import warnings
 
 import torch
-import torch.nn.functional as F
-
 import torch.nn as nn
 import torch.optim as optim
-import torchvision
-from torch.optim import lr_scheduler
 from torchinfo import summary
 from torchvision import datasets, models, transforms
 from torchvision.models.resnet import ResNet18_Weights
@@ -16,7 +12,7 @@ from torchvision.models.resnet import ResNet18_Weights
 warnings.filterwarnings('ignore')
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 torch.manual_seed(42)
-EPOCHS = 25
+EPOCHS = 10
 
 # 1. Preparing the Dataset and Dataloader
 
@@ -41,6 +37,7 @@ inputs, classes = next(iter(train_loader))
 print(inputs.size())
 print(classes)
 
+# 2. Setting the model
 
 model = models.resnet18(weights=ResNet18_Weights.DEFAULT)
 for param in model.parameters():
@@ -52,60 +49,69 @@ model = model.to(device)
 criterion = nn.CrossEntropyLoss()
 
 summary(model)
-# print(inputs.size())
-# print(model(inputs))
-# print(F.softmax(model(inputs), dim=1))
-# print(torch.max(F.softmax(model(inputs), dim=1), dim=1))
-# print(classes[torch.max(F.softmax(model(inputs), dim=1), dim=1)[1]])
-# print(classes[torch.max(F.softmax(model(inputs), dim=1), dim=1)[1]] == classes.data)
-# # print(torch.max(model(inputs), 1)[0].size())
-# # print(criterion(model(inputs), classes))
-# # print(torch.max(model(inputs), 1)[1] == classes.data)
-
-# exit(0)
 
 optimizer_ft = optim.SGD(model.fc.parameters(), lr=0.01, momentum=0.9)
+
+# 3. Building the training  routine
 
 best_acc = 0
 model.train()  # Set model to training mode
 for epoch in range(EPOCHS):
-    print(f'Epoch {epoch}/{EPOCHS - 1}')
+    print(f'Epoch {epoch}/{EPOCHS - 1}\n\n')
     print('-' * 10)
     running_loss = 0.0
     running_corrects = 0
 
     # Iterate over data.
-    for inputs, labels in train_loader:
+    for batch_idx, (inputs, labels) in enumerate(train_loader):
         inputs = inputs.to(device)
         labels = labels.to(device)
 
         # zero the parameter gradients
         optimizer_ft.zero_grad()
 
-        # forward
-        # track history if only in train
-        with torch.enable_grad():
-            predicted_probs = F.softmax(model(inputs), dim=1)
-            class_prediction = classes[torch.max(F.softmax(model(inputs), dim=1), dim=1)[1]]
-            loss = criterion(predicted_probs, labels)
-            loss.backward()
-            optimizer_ft.step()
+        outputs = model(inputs)
+        _, preds = torch.max(outputs, 1)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer_ft.step()
 
         # statistics
         running_loss += loss.item() * inputs.size(0)
-        running_corrects += torch.sum(class_prediction == labels.data)
+        running_corrects += torch.sum(preds == labels.data)
+        if batch_idx % 2 == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(train_set), len(train_loader.dataset),
+                       100. * batch_idx / len(train_loader), loss.item()))
     epoch_loss = running_loss / len(train_set)
     epoch_acc = running_corrects.double() / len(train_set)
 
-    print(f'Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
+    print(f'\nLoss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}\n')
 
     # deep copy the model
     if epoch_acc > best_acc:
         best_acc = epoch_acc
         best_model_wts = copy.deepcopy(model.state_dict())
 
-print(f'Best val Acc: {best_acc:4f}')
+print(f'Best Train Acc: {best_acc:4f}')
 
-model_conv = torchvision.models.resnet18(pretrained=True)
-for param in model_conv.parameters():
-    param.requires_grad = False
+model.eval()
+test_loss = 0
+correct = 0
+for inputs, labels in test_loader:
+    inputs, labels = inputs.to(device), labels.to(device)
+    outputs = model(inputs)
+    _, preds = torch.max(outputs, 1)
+    loss = criterion(outputs, labels)
+    test_loss += loss.item() * inputs.size(0)
+    correct += torch.sum(preds == labels.data)
+
+test_loss /= len(test_loader.dataset)
+
+print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+    test_loss, correct, len(test_loader.dataset),
+    100. * correct / len(test_loader.dataset)))
+
+assert 100. * correct / len(test_loader.dataset) > 60
+
+torch.save(model.state_dict(), "road_signs_cnn.pt")
